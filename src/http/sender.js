0e130b035b664408b8ebef69f3b0f1473dbfc0d8
Kate <import_math@mail.ru>
2023-11-13 23:48:19 +0300

    init

--- src/http/sender.js
+++ src/http/sender.js
@@ -1,6 +1,6 @@
 const axios = require("axios").default;
 
-const serverAddress = "http://localhost:8080";
+const serverAddress = "http://localhost:8081";
 
 export const sendGet = (endpoint, uuid) => {
     
